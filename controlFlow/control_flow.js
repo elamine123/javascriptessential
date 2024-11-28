bb07721ec59402c30e880f7e5d7e4aa1d08c7114
123elamine <1elaminealoui@gmail.com>
2024-11-28 10:49:46 -0500

    the latest update with control flow practice

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,44 @@
+let userRole="Admin";
+let accessLevel;
+
+if(userRole==="Admin"){
+    accessLevel="Full access granted";
+}else if(userRole==="Manager"){
+    accessLevel="Limited access garanted";
+}else{
+    accessLevel="No access garanted";
+}
+
+console.log("access Level :",accessLevel)
+
+let isLoggedIn=true;
+let userMessage;
+
+if(isLoggedIn){
+    if(userRole="Admin"){
+        userMessage="Welcome, Admin!";}
+    else{
+        userMessage="Welcome, User!";
+    }
+}else{
+    userMessage="please log in to access the system.";
+}
+console.log("User Message : ",userMessage);
+
+let userType="Subscriber";
+let userCategory;
+
+switch(userType){
+    case "Admin":
+    userCategory="Administartion";
+    break;
+    case "Manager":
+    userCategory="Manager";
+    break;
+    case "Subscriber":
+    userCategory="Subscriber";
+    break;
+    default:
+        userCategory="Unknown"
+}
+console.log("User Category :",userCategory);
